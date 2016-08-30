bc01e76f2eb282375364e6a47c714d3c82f824ee
Kumar-Shrestha <kmr.shr324@gmail.com>
2016-08-30 21:58:10 +0545

    Login Layout Added

---
+++ app/src/main/java/com/cicnp/reckontech/cicnp/Login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.cicnp.reckontech.cicnp.Login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cicnp.reckontech.cicnp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
